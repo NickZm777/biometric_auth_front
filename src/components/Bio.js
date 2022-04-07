ebdb0eb46c625f2788d2ccdd33448827c349a648
Змурщик Николай <m.zmurshyk@bpcbt.com>
2022-04-07 17:02:25 +0300

    fix2

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -85,19 +85,19 @@ const keyforCheck = async (credential) => {
   })
 }
 
-function publicKeyCredentialToJSON(pubKeyCred) {
-  if (pubKeyCred instanceof ArrayBuffer) {
-    return pubKeyCred
-  } else if (pubKeyCred instanceof Array) {
-    return pubKeyCred.map(publicKeyCredentialToJSON)
-  } else if (pubKeyCred instanceof Object) {
-    const obj = {}
-    for (let key in pubKeyCred) {
-      obj[key] = publicKeyCredentialToJSON(pubKeyCred[key])
-    }
-    return obj
-  } else return pubKeyCred
-}
+// function publicKeyCredentialToJSON(pubKeyCred) {
+//   if (pubKeyCred instanceof ArrayBuffer) {
+//     return pubKeyCred
+//   } else if (pubKeyCred instanceof Array) {
+//     return pubKeyCred.map(publicKeyCredentialToJSON)
+//   } else if (pubKeyCred instanceof Object) {
+//     const obj = {}
+//     for (let key in pubKeyCred) {
+//       obj[key] = publicKeyCredentialToJSON(pubKeyCred[key])
+//     }
+//     return obj
+//   } else return pubKeyCred
+// }
 const domain = document.domain
 const newChallenge = new Uint8Array([21, 31, 105])
 
@@ -157,6 +157,7 @@ const Bio = () => {
   const [getinf, setGetInf] = useState("")
   const [getinff, setGetInff] = useState("")
   const [getinfff, setGetInfff] = useState("")
+  // const [buffer, setBuffer] = useState("")
 
   const createKey = () => {
     if (!window.PublicKeyCredential) {
@@ -168,9 +169,12 @@ const Bio = () => {
     navigator.credentials
       .create({ publicKey })
       .then((output) => {
-        const keyres = publicKeyCredentialToJSON(output)
-        saveKey(keyres)
-        setInf(keyres)
+        // const keyres = publicKeyCredentialToJSON(output)
+        saveKey(output)
+        setInf(output)
+        // const buf = {
+
+        // }
       })
       .catch((error) => {
         console.log("Catch an error in navigator.credentials create:")
@@ -212,3 +216,19 @@ const Bio = () => {
 }
 
 export default Bio
+
+// const igorid = {
+//   rawId: {},
+//   response: {
+//     attestationObject: {},
+//     getAuthenticatorData: {},
+//     getPublicKey: {},
+//     getPublicKeyAlgorithm: {},
+//     getTransports: {},
+//     clientDataJSON: {},
+//   },
+//   authenticatorAttachment: "platform",
+//   getClientExtensionResults: {},
+//   id: "AafxvzWERrz7_y0y0OZ4kHFOh8zLSDJNsloOAkkfK_AVX_2gSx5EgmNrqQxyDYW-MLVrWcy_2DjXm0OVvWYAYy4BTl5Z-yX4geLkh35hCg1b14gO_ruBYwRkON8KK-REausbjzYO2UU",
+//   type: "public-key",
+// }
