2340ca76dd2425dfd6c0e363c562d320469da862
Змурщик Николай <m.zmurshyk@bpcbt.com>
2022-04-12 15:31:49 +0300

    asd

--- src/components/Bio2.js
+++ src/components/Bio2.js
@@ -1,30 +1,80 @@
-import { saveKey, saveBuffer } from "../api/helper";
-import { useState } from "react";
+/* eslint-disable no-unused-vars */
+import { saveKey, getInitChallenge, saveBuffer } from "../api/helper"
+import { useState } from "react"
+const base64 = require("base-64")
+
 // import { uuid } from "uuidv4"
 // import { base64urlEncode } from "base64url"
 
-function publicKeyCredentialToJSON(pubKeyCred) {
-  if (pubKeyCred instanceof ArrayBuffer) {
-    return window.atob(pubKeyCred);
-  } else if (pubKeyCred instanceof Array) {
-    return pubKeyCred.map(publicKeyCredentialToJSON);
-  } else if (pubKeyCred instanceof Object) {
-    const obj = {};
-    for (let key in pubKeyCred) {
-      obj[key] = publicKeyCredentialToJSON(pubKeyCred[key]);
-    }
-    return obj;
-  } else return pubKeyCred;
+const decode = (buffer, utf) => {
+  return new TextDecoder(utf).decode(buffer)
 }
 
+const encode = (string) => {
+  return new TextEncoder().encode(string)
+}
+
+const keyforCheck = async (credential) => {
+  return await navigator.credentials.get({
+    publicKey: {
+      challenge: null,
+      allowCredentials: [
+        {
+          id: credential.rawId,
+          type: "public-key",
+          transports: ["internal"],
+        },
+      ],
+      rpId: domain,
+      // attachment: "platform",
+      userVerification: "required",
+    },
+  })
+}
+
+function arrayBufferToBase64(ab) {
+  var dView = new Uint8Array(ab) //Get a byte view
+
+  var arr = Array.prototype.slice.call(dView) //Create a normal array
+
+  var arr1 = arr.map(function (item) {
+    return String.fromCharCode(item) //Convert
+  })
+
+  return window.btoa(arr1.join("")) //Form a string
+}
+
+function publicKeyCredentialToJSON(pubKeyCred) {
+  if (pubKeyCred instanceof ArrayBuffer) {
+    return arrayBufferToBase64(pubKeyCred)
+  } else if (pubKeyCred instanceof Array) {
+    return pubKeyCred.map(publicKeyCredentialToJSON)
+  } else if (pubKeyCred instanceof Object) {
+    const obj = {}
+    for (let key in pubKeyCred) {
+      obj[key] = publicKeyCredentialToJSON(pubKeyCred[key])
+    }
+    return obj
+  } else return pubKeyCred
+}
+
+const domain = document.domain
+
+const iserID = new Uint8Array([21, 31, 105])
+
 const Bio2 = () => {
-  const [inf, setInf] = useState("");
+  const [inf, setInf] = useState("")
+  const [getinf, setGetInf] = useState("")
+  const [getinff, setGetInff] = useState("")
+  const [getinfff, setGetInfff] = useState("")
+  // const [buffer, setBuffer] = useState("")
+  const [initChallenge, setInitChallenge] = useState("")
 
   const publicKey = {
     challenge: null,
-    rp: { id: document.domain, name: "My test TouchID" },
+    rp: { id: domain, name: "My test TouchID" },
     user: {
-      id: new Uint8Array([21, 31, 105]),
+      id: iserID,
       name: "jason.x@.pl",
       displayName: "Jason X",
     },
@@ -38,46 +88,92 @@ const Bio2 = () => {
       authenticatorAttachment: "platform",
       userVerification: "required",
     },
-  };
+  }
 
   const createKey = async () => {
     if (!window.PublicKeyCredential) {
-      console.log("window.PublicKeyCredential is false");
-      return;
+      alert("window.PublicKeyCredential is false")
+      return
     }
-    const challenge = "Bio2challenge";
-
-    publicKey.challenge = Uint8Array.from(challenge);
+    const serverChallengeString = await getInitChallenge()
+    publicKey.challenge = encode(serverChallengeString)
 
     await navigator.credentials
       .create({ publicKey })
       .then((output) => {
-        saveBuffer({
-          buffertype: "Uint8Array.from(challenge)-Bio2",
-          output: output,
-        });
-        const keyres = publicKeyCredentialToJSON(output);
-        saveKey(keyres);
-        setInf(keyres);
+        const keyres = publicKeyCredentialToJSON(output)
+        saveKey(keyres)
+        setInf(keyres)
       })
       .catch((error) => {
-        console.log("Catch an error in navigator.credentials create:");
-        console.log(error.message);
-        setInf(error.message);
-      });
-  };
+        alert("Catch an error in navigator.credentials create:", error.message)
+        setInf(error.message)
+      })
+  }
+  const trykeyforCheck = (inf) => {
+    keyforCheck(inf)
+      .then((output) => {
+        // const keyres = publicKeyCredentialToJSON(output)
+        saveKey(output)
+        setGetInf(output)
+        setGetInff(typeof output)
+      })
+      .catch((error) => {
+        alert("Catch an error in navigator.credentials create:", error.message)
+      })
+  }
+
+  const getStartChallenge = () => {
+    getInitChallenge()
+      .then((res) => {
+        setInitChallenge(res)
+        // console.log(JSON.stringify(newChallenge) === JSON.stringify(res))
+        return res
+      })
+      .catch((error) => {
+        console.log(error.message)
+      })
+  }
 
   return (
     <div>
       <button className="btn-bio" onClick={() => createKey()}>
-        Bio2
+        Touch ID
       </button>
       <div className="inf-bio">{JSON.stringify(inf)}</div>
+      <button className="btn-bio" onClick={() => trykeyforCheck(inf)}>
+        check by Touch IDE
+      </button>
+      <button className="btn-bio" onClick={() => getStartChallenge()}>
+        initChallenge
+      </button>
+      <div className="inf-bio">{JSON.stringify(getinf)}</div>
+      <div className="inf-bio">{JSON.stringify(getinff)}</div>
+      <div className="inf-bio">{JSON.stringify(getinfff)}</div>
+
+      {/* <span>challenge:</span>
+      <span>{JSON.stringify(initChallenge)}</span> */}
     </div>
-  );
-};
+  )
+}
 
 // const encodedData = window.btoa("Hello, world")
 // const decodedData = window.atob(encodedData)
 
-export default Bio2;
+export default Bio2
+
+// const igorid = {
+//   rawId: {},
+//   response: {
+//     attestationObject: {},
+//     getAuthenticatorData: {},
+//     getPublicKey: {},
+//     getPublicKeyAlgorithm: {},
+//     getTransports: {},
+//     clientDataJSON: {},
+//   },
+//   authenticatorAttachment: "platform",
+//   getClientExtensionResults: {},
+//   id: "AafxvzWERrz7_y0y0OZ4kHFOh8zLSDJNsloOAkkfK_AVX_2gSx5EgmNrqQxyDYW-MLVrWcy_2DjXm0OVvWYAYy4BTl5Z-yX4geLkh35hCg1b14gO_ruBYwRkON8KK-REausbjzYO2UU",
+//   type: "public-key",
+// }
