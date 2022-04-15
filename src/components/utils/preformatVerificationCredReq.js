bc9a69b0a892f1b2ead156cd0fd43e5e6fa52e79
Змурщик Николай <m.zmurshyk@bpcbt.com>
2022-04-15 17:04:56 +0300

    sdf

--- src/components/utils/preformatVerificationCredReq.js
+++ src/components/utils/preformatVerificationCredReq.js
@@ -7,6 +7,7 @@ const preformatVerificationCredReq = (verifyCredReq, domain) => {
   // makeCredReq.challenge = base64url.decode(makeCredReq.challenge);
   // makeCredReq.user.id = base64url.decode(makeCredReq.user.id);
 
+  verifyCredReq.challenge = encode(verifyCredReq.challenge)
   verifyCredReq.allowCredentials[0].id = encode(
     verifyCredReq.allowCredentials[0].id
   )
