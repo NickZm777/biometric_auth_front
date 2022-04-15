4e4937de4667727d73d09fda38e1b5d6295787bf
Змурщик Николай <m.zmurshyk@bpcbt.com>
2022-04-15 16:33:20 +0300

    sd

--- src/api/endpoint.js
+++ src/api/endpoint.js
@@ -1,5 +1,5 @@
 //
-// const BASE_URL = "https://glowing-kringle-b3a3c5.netlify.app"
-const BASE_URL = "http://localhost:9000"
+const BASE_URL = "https://glowing-kringle-b3a3c5.netlify.app"
+// const BASE_URL = "http://localhost:9000"
 
 export default BASE_URL
