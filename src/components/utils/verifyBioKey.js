b6532630b08021712683903f881507f25ad55321
Змурщик Николай <m.zmurshyk@bpcbt.com>
2022-04-18 16:37:05 +0300

    as

--- src/components/utils/verifyBioKey.js
+++ src/components/utils/verifyBioKey.js
@@ -7,7 +7,7 @@ const verifyBioKey = async ({ publicKey }) => {
   //   console.log("window.PublicKeyCredential is disabled")
   //   return
   // }
-  alert(publicKey)
+  alert(JSON.stringify(publicKey))
   const result = await navigator.credentials
     .get({ publicKey: publicKey })
     .then((output) => {
