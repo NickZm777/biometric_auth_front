3446108d6aa1f26fe239ab6ef3cf1517c1b68002
Змурщик Николай <m.zmurshyk@bpcbt.com>
2022-04-20 18:12:50 +0300

    asd

--- src/components/Bio2.js
+++ src/components/Bio2.js
@@ -65,7 +65,7 @@ async function getCR(id) {
           {
             type: "public-key",
             id: new TextEncoder().encode(id).buffer,
-            transports: ["internal"],
+            // transports: ["internal"],
           },
         ],
         userVerification: "required",
